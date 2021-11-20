5597c140ed09fe0852e8abf13e834304ca012a1d
toptaskalender <toptaskalender@gmail.com>
2021-11-20 13:02:16 +0300

    Add route for '/v1/tours'

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require('express');
 const app   = express();
 const port  = 3000;
 
+const tours = JSON.parse( fs.readFileSync('./dev-data/data/tours-simple.json', 'utf8') );
+
+app.get('/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
 });
