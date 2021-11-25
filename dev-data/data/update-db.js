301ff13a51fca761b7263e597bd1b0380cee59c5
toptaskalender <toptaskalender@gmail.com>
2021-11-25 14:36:18 +0300

    Create script for deleting and inserting all tours via cli

---
+++ dev-data/data/update-db.js
@@ -0,0 +1,54 @@
+const fs        = require('fs');
+const mongoose  = require('mongoose');
+require('dotenv').config();
+const { Tour }  = require('../../models');
+
+/* DB Connection */
+const db                = process.env.DATABASE_CONNECTION;
+const connectionString  = db.replace('<PASSWORD>', process.env.DATABASE_PASSWORD); 
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+    console.log(`Database connection: connected 🖐`);
+  })
+  .catch((err) => {
+    console.error(`Database connection error: ${err}`);
+  })
+
+/* Args */
+const args = process.argv.slice(2);
+
+/* Read Tours */
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8'));
+
+/* Functions  */
+async function deleteAllTours() {
+  try {
+    await Tour.deleteMany();
+    console.log('All tours successfully deleted ✅');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(1);
+}
+
+async function importAllTours() {
+  try {
+    await Tour.create(tours);
+    console.log('All tours successfully inserted ✅');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(1);
+}
+
+/* Decision */
+if (args[0] === '--delete') {
+  deleteAllTours();
+} else if (args[0] === '--insert') {
+  importAllTours();
+}
