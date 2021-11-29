6679246c9742ceae07b027024e2653c6039e2dcb
toptaskalender <toptaskalender@gmail.com>
2021-11-29 11:19:29 +0300

    Update the way middlewares get exported

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+const {
+  errorController
+}                         = require('./error');
+const {
+  createQueryTop5Ratings
+}                         = require('./tours');
+
+module.exports = {
+  errorController,
+  createQueryTop5Ratings
+}
