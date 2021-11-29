92d4f0f6e3ce1aa123c83cf04befe8e7316deecd
toptaskalender <toptaskalender@gmail.com>
2021-11-29 11:42:22 +0300

    Update AppError class

--- utils/app-error.js
+++ utils/app-error.js
@@ -2,9 +2,9 @@ class AppError extends Error {
   constructor(errorMsg, statusCode) {
     super(errorMsg);
 
-    this.statusCode = statusCode;
-    this.status     = String(statusCode).startsWith('4') ? 'fail' : 'error'; 
-
+    this.statusCode     = statusCode;
+    this.status         = String(statusCode).startsWith('4') ? 'fail' : 'error'; 
+    this.isOperational  = true;
   }
 }
 
