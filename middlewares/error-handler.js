458644d7b8993e9787f25a284e985e79cb757de1
toptaskalender <toptaskalender@gmail.com>
2021-11-29 21:15:10 +0300

    Update global error-handler middleware

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,14 +1,43 @@
-function errorHandler(err, req, res, next) {
+const environment = process.env.NODE_ENV;
 
-  if (!err.isOperational) {
-    err.statusCode  = 500;
-    err.status      = 'error';
+function sendProdError(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong❗'
+    });
   }
+}
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+function sendDevError(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      error: err,
+      status: err.status,
+      message: err.message,
+      stack: err.stack
+    });
+  } else {
+    res.status(500).json({
+      error: err,
+      status: 'error',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  if (environment === 'development') {
+    sendDevError(err, res);
+  } else if (environment === 'production') {
+    sendProdError(err, res);
+  }
 }
 
 module.exports = {
