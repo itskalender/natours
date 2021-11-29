310ac0d5f1870353ccafa63b26ecb8f964a418c9
toptaskalender <toptaskalender@gmail.com>
2021-11-29 11:19:56 +0300

    Delete deprecated middleware

--- middlewares/tours.js
+++ middlewares/tours.js
@@ -7,18 +7,6 @@ function createQueryTop5Ratings(req, res, next) {
   next();
 }
 
-function checkTourId(_, res, next, val) {
-  if (val > tours.length - 1) {
-    return res.status(404).json({
-      status: 'fail',
-      data: 'Cannot find a tour with this id❗'
-    })
-  }
-
-  next();
-}
-
 module.exports = {
   createQueryTop5Ratings,
-  checkTourId,
 }
