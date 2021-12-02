8eaf6e03715af4934b1feb1fd9f26b9c905701cd
toptaskalender <toptaskalender@gmail.com>
2021-12-02 12:01:22 +0300

    Update getUsers controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,19 @@
-const getUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not been created yet ❗'
-  })
-}
+const { userService } = require('../services');
+const {
+  catchAsync
+}                     = require('../utils');
+
+const getUsers = catchAsync(async (req, res) => {
+  const users = await userService.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
+  });
+});
 
 const getUser = (req, res) => {
   res.status(500).json({
