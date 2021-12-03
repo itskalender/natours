d285b903f3f50f46c4ff3a71ec63f062b7fb667f
toptaskalender <toptaskalender@gmail.com>
2021-12-03 10:27:06 +0300

    Create 'restrictTo' middleware to check user's permissions

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const errorHandler            = require('./error-handler');
 const createQueryTop5Ratings  = require('./create-query-top-five-ratings');
 const verifyAuth              = require('./verify-auth');
+const restrictTo              = require('./restrict-to');
 
 module.exports = {
   errorHandler,
   createQueryTop5Ratings,
-  verifyAuth
+  verifyAuth,
+  restrictTo
 }
---
+++ middlewares/restrict-to.js
@@ -0,0 +1,16 @@
+const { AppError } = require('../utils');
+
+function restrictTo(...requiredRoles) {
+  return (req, _, next) => {
+    const { user }      = req;
+    const isUserAllowed = requiredRoles.includes(user.role);
+
+    if (!isUserAllowed) {
+      return next(new AppError('You are not allowed to perform this action.', 403));
+    }
+
+    next();
+  }
+}
+
+module.exports = restrictTo;
