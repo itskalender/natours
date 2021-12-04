7f50d773d685c12d1880b303b0bb51a201953258
toptaskalender <toptaskalender@gmail.com>
2021-12-04 16:35:00 +0300

    Change req limit per hour

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 2,
+  max: 100,
   message: 'Too many requests performed from this IP, please try again after an hour'
 });
 
