0726434f205192f56955af62f2fae2389461acf6
toptaskalender <toptaskalender@gmail.com>
2021-12-07 19:01:40 +0300

    Update validate middleware func to have the ability to handle multiple source param

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -2,8 +2,23 @@ const { AppError } = require('../utils');
 
 function validate(source, schema) {
   return (req, _, next) => {
-    const data                = req[source];
-    const { error, value }    = schema.validate(data);
+    let data;
+    
+    const isTypeOfSourceEqualToString = typeof source === 'string';
+    const isTypeofSourceEqualToArray  = Array.isArray(source) === true;
+
+    if (isTypeOfSourceEqualToString) {
+      data = req[source];
+    } else if (isTypeofSourceEqualToArray) {
+      source.forEach(s => {
+        data = { 
+          ...data,
+          ...req[s]
+        };
+      });
+    }
+
+    const { error, value }  = schema.validate(data);
 
     if (error) {
       const msg = `${error.details.map(err => err.message).join(', ')}.`;
