6ef4fdaa209b6174fd85f95f387f7540cfc9985e
toptaskalender <toptaskalender@gmail.com>
2021-12-07 16:21:33 +0300

    Export 'validate' middleware func

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,11 +3,13 @@ const createQueryTop5Ratings  = require('./create-query-top-five-ratings');
 const verifyAuth              = require('./verify-auth');
 const restrictTo              = require('./restrict-to');
 const limiter                 = require('./limiter');
+const validate                = require('./validate');
 
 module.exports = {
   errorHandler,
   createQueryTop5Ratings,
   verifyAuth,
   restrictTo,
-  limiter
+  limiter,
+  validate
 }
