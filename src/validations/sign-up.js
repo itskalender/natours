ab24f2302b5b246fafe733881a2250af3655c0e9
toptaskalender <toptaskalender@gmail.com>
2021-12-07 16:20:18 +0300

    Create sign-up validation schema

---
+++ src/validations/sign-up.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+const signUpSchema = Joi.object({
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .lowercase(),
+
+  password: Joi.string()
+    .required()
+    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+  passwordConfirm: Joi.ref('password'),
+
+  name: Joi.string()
+    .required()
+    .trim()
+    .min(2)
+    .max(20),
+  
+  role: Joi.string()
+    .allow('user', 'guide', 'lead-guide', 'admin')
+    .default('user')
+});
+
+module.exports = signUpSchema;
