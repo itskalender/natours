3f942cb52ae53325b42bd2db0e362d19076ae82f
toptaskalender <toptaskalender@gmail.com>
2021-12-07 19:03:25 +0300

    Export resetPasswordSchema

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,11 @@
 const signUpSchema          = require('./sign-up');
 const logInSchema           = require('./log-in');
 const forgotPasswordSchema  = require('./forgot-password');
+const resetPasswordSchema   = require('./reset-password');
 
 module.exports = {
   signUpSchema,
   logInSchema,
-  forgotPasswordSchema
+  forgotPasswordSchema,
+  resetPasswordSchema
 }
